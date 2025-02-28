de13041b741f357e278fa32f675a0a876c34e788
DylanDoe21 <dylandutes@gmail.com>
2025-02-27 20:15:13 -0500

    Trawler armor bonus

--- Items/Armor/Trawler/TrawlerHat.cs
+++ Items/Armor/Trawler/TrawlerHat.cs
@@ -2,6 +2,8 @@ using WaasephisFishingPlus.Items.Filleting;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
 
 namespace WaasephisFishingPlus.Items.Armor.Trawler
 {
@@ -12,15 +14,15 @@ namespace WaasephisFishingPlus.Items.Armor.Trawler
 		public override void SetStaticDefaults()
 		{
 			base.SetStaticDefaults();
-            ArmorIDs.Head.Sets.DrawHatHair[EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head)] = true;
-        }
+			ArmorIDs.Head.Sets.DrawHatHair[EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head)] = true;
+		}
 
 		public override void SetDefaults()
 		{
 			Item.width = 24;
 			Item.height = 14;
 			Item.value = Item.sellPrice(gold: 2, silver: 50);
-            Item.rare = ItemRarityID.LightRed;
+			Item.rare = ItemRarityID.LightRed;
 			Item.defense = 11;
 		}
 
@@ -35,8 +37,9 @@ namespace WaasephisFishingPlus.Items.Armor.Trawler
 		}
 
 		public override void UpdateArmorSet(Player player)
-		{
-			player.setBonus = "Greatly reduced enemy spawn rate";
+		{	
+			player.setBonus = "Greatly reduced enemy spawn rate"; //this should probably be localized
+			player.GetModPlayer<TrawlerSetbonusPlayer>().SetBonus = true;
 			player.anglerSetSpawnReduction = true;
 			player.calmed = true;
 		}
@@ -44,10 +47,33 @@ namespace WaasephisFishingPlus.Items.Armor.Trawler
 		public override void AddRecipes()
 		{
 			Recipe recipe = CreateRecipe();
-            recipe.AddIngredient(ItemID.AncientCloth, 5);
-            recipe.AddIngredient(ItemID.AnglerHat);
-            recipe.AddTile(TileID.Loom);
-            recipe.Register();
+			recipe.AddIngredient(ItemID.AncientCloth, 5);
+			recipe.AddIngredient(ItemID.AnglerHat);
+			recipe.AddTile(TileID.Loom);
+			recipe.Register();
+		}
+	}
+
+	public class TrawlerSetbonusPlayer : ModPlayer
+	{
+		public bool SetBonus = false;
+
+		public override void ResetEffects()
+		{
+			SetBonus = false;
+		}
+	}
+
+	public class TrawlerItemGlobal : GlobalItem
+	{
+		public override bool Shoot(Item item, Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			if (item.fishingPole > 0 && player.GetModPlayer<TrawlerSetbonusPlayer>().SetBonus)
+			{
+				Projectile.NewProjectile(source, position, velocity + new Vector2(2 * player.direction, 0), type, damage, knockback, player.whoAmI);
+			}
+
+			return base.Shoot(item, player, source, position, velocity, type, damage, knockback);
 		}
 	}
 }
