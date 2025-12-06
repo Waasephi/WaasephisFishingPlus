0a12311e04536df93740b045ed3e4531a932cea8
Waasephi <34556008+Waasephi@users.noreply.github.com>
2025-12-05 18:45:02 -0600

    Clam Minion
    clam hour is nowr

---
+++ Content/Items/Weapons/Summon/ClamTotem.cs
@@ -0,0 +1,69 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WaasephisFishingPlus.Content.Items.Materials;
+using WaasephisFishingPlus.Content.Projectiles.Weapons.Summon.Minions;
+using WaasephisFishingPlus.Content.Tiles.Decor;
+
+namespace WaasephisFishingPlus.Content.Items.Weapons.Summon
+{
+    public class ClamTotem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            ItemID.Sets.GamepadWholeScreenUseRange[Item.type] = true;
+            ItemID.Sets.LockOnIgnoresCollision[Item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 23;
+            Item.DamageType = DamageClass.Summon;
+            Item.mana = 10;
+            Item.width = 32;
+            Item.height = 38;
+            Item.useTime = 36;
+            Item.useAnimation = 36;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.noMelee = true;
+            Item.knockBack = 3;
+            Item.value = Item.sellPrice(0, 1, 0, 0);
+            Item.rare = ItemRarityID.Orange;
+            Item.UseSound = SoundID.Item44;
+            Item.buffType = ModContent.BuffType<ClamMinionBuff>();
+            Item.shoot = ModContent.ProjectileType<ClamMinionProj>();
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            // Here you can change where the minion is spawned. Most vanilla minions spawn at the cursor position
+            position = Main.MouseWorld;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            // This is needed so the buff that keeps your minion alive and allows you to despawn it properly applies
+            player.AddBuff(Item.buffType, 2);
+
+            // Minions have to be spawned manually, then have originalDamage assigned to the damage of the summon item
+            var projectile = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
+            projectile.originalDamage = Item.damage;
+
+            // Since we spawned the projectile manually already, we do not need the game to spawn it for ourselves anymore, so return false
+            return false;
+        }
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+			.AddIngredient(ModContent.ItemType<ClamShell>(), 12)
+			.AddIngredient(ItemID.WhitePearl, 2)
+			.AddTile(ModContent.TileType<Shellforge>())
+			.Register();
+		}
+	}
+}
---
+++ Content/Projectiles/Weapons/Summon/Minions/ClamMinionProj.cs
@@ -0,0 +1,101 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WaasephisFishingPlus.Content.Projectiles.Weapons.Summon.Minions
+{
+
+    public class ClamMinionBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoSave[Type] = true; // This buff won't save when you exit the world
+            Main.buffNoTimeDisplay[Type] = true; // The time remaining won't display on this buff
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            // If the minions exist reset the buff time, otherwise remove the buff from the player
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<ClamMinionProj>()] > 0)
+            {
+                player.buffTime[buffIndex] = 18000;
+            }
+            else
+            {
+                player.DelBuff(buffIndex);
+                buffIndex--;
+            }
+        }
+    }
+    public class ClamMinionProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 6;
+            // This is necessary for right-click targeting
+            ProjectileID.Sets.MinionTargettingFeature[Projectile.type] = true;
+
+            Main.projPet[Projectile.type] = true; // Denotes that this projectile is a pet or minion
+
+            ProjectileID.Sets.MinionSacrificable[Projectile.type] = true; // This is needed so your minion can properly spawn when summoned and replaced when other minions are summoned
+            ProjectileID.Sets.CultistIsResistantTo[Projectile.type] = true; // Make the cultist resistant to this projectile, as it's resistant to all homing projectiles.
+        }
+
+		public sealed override void SetDefaults()
+		{
+			Projectile.CloneDefaults(ProjectileID.BabySlime);
+			Projectile.width = 24;
+			Projectile.height = 16;
+			Projectile.tileCollide = false;
+
+			Projectile.friendly = true;
+			Projectile.minion = true;
+			Projectile.DamageType = DamageClass.Summon;
+			Projectile.minionSlots = 1f;
+			Projectile.penetrate = -1;
+			AIType = ProjectileID.BabySlime;
+		}
+
+		public override bool? CanCutTiles()
+		{
+			return false;
+		}
+
+		public override bool MinionContactDamage()
+		{
+			return true;
+		}
+
+		public override void AI()
+		{
+			Player owner = Main.player[Projectile.owner];
+
+			if (!CheckActive(owner))
+			{
+				return;
+			}
+		}
+
+		private bool CheckActive(Player owner)
+		{
+			if (owner.dead || !owner.active)
+			{
+				owner.ClearBuff(ModContent.BuffType<ClamMinionBuff>());
+
+				return false;
+			}
+
+			if (owner.HasBuff(ModContent.BuffType<ClamMinionBuff>()))
+			{
+				Projectile.timeLeft = 2;
+			}
+
+			return true;
+		}
+		public override Color? GetAlpha(Color lightColor)
+		{
+			return new Color(255, 255, 255, 255);
+		}
+	}
+}
